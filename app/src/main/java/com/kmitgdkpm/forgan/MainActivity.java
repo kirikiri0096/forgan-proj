b1421675711f4d749264d68603705fa331739b32
zeegame <zee.chavit@gmail.com>
2018-11-05 22:22:50 +0700

    test

--- app/src/main/java/com/kmitgdkpm/forgan/MainActivity.java
+++ app/src/main/java/com/kmitgdkpm/forgan/MainActivity.java
@@ -4,16 +4,23 @@ import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity  {
+    private RadioButton usrDB, usrAuth, usrRegEmail, usrRegGoo, usrRegFace, sysEtc, usrPictureUpload, usrUpdateProf, usrGetProf, supGloLst, usrMakeOrder;
+    private Button goBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
     public void debugger (View view){
         Intent intent = new Intent(this, Usermain.class);
         startActivity(intent);
     }
+
 }
